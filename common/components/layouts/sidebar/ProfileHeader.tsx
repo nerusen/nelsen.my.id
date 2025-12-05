a7fcbc7580b80dc5c19f99f0a6cc403a4b9de270
nerusen <162538024+nerusen@users.noreply.github.com>
2025-12-05 19:22:33 +0700

    Update profile image source in ProfileHeader

--- common/components/layouts/sidebar/ProfileHeader.tsx
+++ common/components/layouts/sidebar/ProfileHeader.tsx
@@ -23,7 +23,7 @@ const ProfileHeader = ({ expandMenu, imageSize }: ProfileHeaderProps) => {
     >
       <div className="google-profile-border">
         <Image
-          src={"/images/satria-1.jpg"}
+          src={"/images/nelsen-1.png"}
           width={expandMenu ? 80 : imageSize * 0.9}
           height={expandMenu ? 80 : imageSize * 0.9}
           alt="n31sen.st"
