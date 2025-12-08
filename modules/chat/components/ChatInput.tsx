245ec6172e26edec2fbbe3e870a7d87b77c13dd2
nerusen <162538024+nerusen@users.noreply.github.com>
2025-12-09 00:39:54 +0700

    Add files via upload

--- modules/chat/components/ChatInput.tsx
+++ modules/chat/components/ChatInput.tsx
@@ -39,6 +39,8 @@ const ChatInput = ({
 
   const t = useTranslations("ChatRoomPage");
 
+  const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB in bytes
+
   const handleSendMessage = (e: FormEvent) => {
     e.preventDefault();
 
